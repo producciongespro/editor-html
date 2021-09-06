17c7b3c626452dd41e2703e2eb006018c3ef3356
lchaconc <luis.chacon.campos@gmail.com>
2021-09-06 13:56:24 -0600

    Pruba de inyección html -Carga de data -Render de propiedad cuerpo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import  { useState, useEffect } from 'react';
 
-const cargarDatos = async ()=> {
-  let res = await fetch ("./data/revistas.json");
-  res = await res.json();  
-  console.log(res);
-  return res;
-}
 
 
-function App() {
 
-const revistas = cargarDatos();
-console.log(revistas);
+ function App() {
+
+
+  const [revistas, setRevistas] = useState(null);
+
+  const cargarDatos = async ()=> {
+    let res = await fetch ("./data/revistas.json");
+    res = await res.json();    
+    setRevistas(res);
+  }
+
+  useEffect(()  =>  {
+    cargarDatos();
+  }, []);
+
+  useEffect(() => {
+    console.log(revistas);
+  }, [revistas]);
+
 
 
   return (
-    <h1>Test</h1>
+    <div className="container">
+        <h1>Test</h1>
+        {
+          revistas[0].cuerpo
+        }
+    </div>
   )
 
 
