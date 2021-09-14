578d03525514ec9e5f57852ba73df902b57725b0
lchaconc <luis.chacon.campos@gmail.com>
2021-09-14 14:32:13 +0000

    Render de etiquetas en text area -Imp slice con indice -Carga de output1 y 2 concatenación y render en text area

--- src/componentes/Editor.jsx
+++ src/componentes/Editor.jsx
@@ -7,15 +7,29 @@ export default function Editor(props) {
     const refContainer = useRef(null);
 
     const handleSetHeader =(e)=> {
-        let txtEditor = refContainer.current;
+        const openTag = "<H1>";
+        const closeTag = "</H1> ";
+        const txtEditor = refContainer.current;
         const start = txtEditor.selectionStart;
         // obtain the index of the last selected character
         const finish = txtEditor.selectionEnd;
+
+        let texto = txtEditor.value;
+        const output1 = [texto.slice(0, finish - 1), closeTag, texto.slice(finish)  ].join('');
+        const output2 = [output1.slice(0, start), openTag, output1.slice(start)  ].join('');
+        
+        //console.log(output1);
+        console.log(output2);
+        txtEditor.value = output2;
+
         // obtain the selected text
+        /*
+
         const sel = txtEditor.value.substring(start, finish);
         console.log(sel);
         console.log(start, "--", finish);
-        //console.log(texto);
+        console.log(texto);
+        */
     }
 
   return (
@@ -28,7 +42,7 @@ export default function Editor(props) {
 
       <div className="row">
         <div className="col-sm-12" >
-          <textarea ref={refContainer} id="txtEditor" cols="30" rows="10"></textarea>
+          <textarea className="form-control" ref={refContainer} id="txtEditor" cols="30" rows="10"></textarea>
         </div>
       </div>
     </>
