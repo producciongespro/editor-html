238e52fffbc3d76809654c25096352c28c86486a
lchaconc <luis.chacon.campos@gmail.com>
2021-09-14 14:54:11 -0600

    CArga de arreglo de botones -Params mediante id de btn

--- src/componentes/Editor.jsx
+++ src/componentes/Editor.jsx
@@ -1,48 +1,47 @@
-import React, {  useRef } from 'react';
-
-
-
+import React, { useRef } from "react";
+import * as utils from "../utils/utils";
 
 export default function Editor(props) {
-    const refContainer = useRef(null);
+  const refContainer = useRef(null);
 
-    const handleSetHeader =(e)=> {
-        const openTag = "<H1>";
-        const closeTag = "</H1> ";
-        const txtEditor = refContainer.current;
-        const start = txtEditor.selectionStart;
-        // obtain the index of the last selected character
-        const finish = txtEditor.selectionEnd;
-
-        let texto = txtEditor.value;
-        const output1 = [texto.slice(0, finish - 1), closeTag, texto.slice(finish)  ].join('');
-        const output2 = [output1.slice(0, start), openTag, output1.slice(start)  ].join('');
-        
-        //console.log(output1);
-        console.log(output2);
-        txtEditor.value = output2;
-
-        // obtain the selected text
-        /*
-
-        const sel = txtEditor.value.substring(start, finish);
-        console.log(sel);
-        console.log(start, "--", finish);
-        console.log(texto);
-        */
-    }
+  const handleSetHeader = (e) => { 
+    utils.setHeader(e.target.id, refContainer );
+  };
 
   return (
     <>
       <div className="row mb-2">
-          <div className="col-12">
-              <button className="btn btn-outline-success" onClick={handleSetHeader} >H1</button>
-          </div>
+        <div
+          className="col-3 btn-group"
+          role="group"
+          aria-label="Botones comandos"
+        >
+          <button
+            id="h1"
+            type="button"
+            onClick={handleSetHeader}
+            className="btn btn-outline-primary"
+          >
+            H1
+          </button>
+          <button id="h2"  onClick={handleSetHeader} type="button" className="btn btn-outline-primary">
+            H2
+          </button>
+          <button id="h3"  onClick={handleSetHeader} type="button" className="btn btn-outline-primary">
+            H3
+          </button>
+        </div>
       </div>
 
       <div className="row">
-        <div className="col-sm-12" >
-          <textarea className="form-control" ref={refContainer} id="txtEditor" cols="30" rows="10"></textarea>
+        <div className="col-sm-12">
+          <textarea
+            className="form-control"
+            ref={refContainer}
+            id="txtEditor"
+            cols="30"
+            rows="10"
+          ></textarea>
         </div>
       </div>
     </>
---
+++ src/utils/utils.js
@@ -0,0 +1,26 @@
+export function setHeader (id, ref  ) {
+
+    const openTag =  `<${id}>`;
+    const closeTag = `</${id}>`;
+    const txtEditor = ref.current;
+    const start = txtEditor.selectionStart;
+    // obtain the index of the last selected character
+    const finish = txtEditor.selectionEnd;
+
+    let texto = txtEditor.value;
+    const output1 = [
+      texto.slice(0, finish - 1),
+      closeTag,
+      texto.slice(finish),
+    ].join("");
+    const output2 = [
+      output1.slice(0, start),
+      openTag,
+      output1.slice(start),
+    ].join("");
+
+    //console.log(output1);
+    console.log(output2);
+    txtEditor.value = output2;
+
+}
