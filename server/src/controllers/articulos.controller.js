e56330320de1a2506cb27f2fabfd019545ebfc08
lchaconc <luis.chacon.campos@gmail.com>
2021-09-20 14:29:04 -0600

    Creación de controlador update -Creación de logica -ref en ruter -Pruebar de envio de datos

--- server/src/controllers/articulos.controller.js
+++ server/src/controllers/articulos.controller.js
@@ -7,11 +7,23 @@ export async function getArticulos(req, res) {
 }
 
 
-export async function setArticulos(req, res) {      
+export async function setArticulo(req, res) {      
     const {titulo, volumen, anno, autor, cuerpo} = req.body;
     const nuevoArticulo = new Articulo({titulo, volumen, anno, autor, cuerpo});
     await nuevoArticulo.save();
     
 
     res.json({isOk:true});
+}
+
+
+export async function updateArticulo (req, res) {
+    const { _id } = req.query;
+    const {titulo, volumen, anno, autor, cuerpo} = req.body;
+    //console.log( _id);
+    //console.log(titulo, volumen, anno, autor, cuerpo);
+    await Articulo.findByIdAndUpdate( _id, { titulo, volumen, anno, autor, cuerpo } );
+
+    res.json({isOk: true});
+
 }
--- server/src/routes/articulos.router.js
+++ server/src/routes/articulos.router.js
@@ -3,7 +3,8 @@ import * as ctrArticulos from "../controllers/articulos.controller.js";
 const routerArticulos = Router();
 
 routerArticulos.get("/articulos", ctrArticulos.getArticulos);
-routerArticulos.post ("/articulos", ctrArticulos.setArticulos);
+routerArticulos.post ("/articulos", ctrArticulos.setArticulo);
+routerArticulos.put ("/articulos", ctrArticulos.updateArticulo);
 
 
 export default routerArticulos;
